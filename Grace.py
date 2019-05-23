caa7537e6fc7b0652d4802bf88b75092b296ce86
n0n3x1573n7 <n0n3x1573n7@users.noreply.github.com>
2019-05-23 23:57:50 +0900

    Update Grace.py
    version diff

--- Grace.py
+++ Grace.py
@@ -14,7 +14,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("---------------")
-    await client.change_presence(game=discord.Game(name='>>', type=1))
+    await client.change_presence(activity=discord.Game(name='>>', type=1))
 
 
 @client.event
