084b794a4f8692138317e439ae4e99e9b13f0868
n0n3x1573n7 <n0n3x1573n7@users.noreply.github.com>
2019-06-12 12:14:18 +0900

    Update Grace_game.py

--- Grace_game.py
+++ Grace_game.py
@@ -117,6 +117,10 @@ async def 내전개최(message):
         minute=int(time[1])
     time=datetime(year=current.year, month=current.month, day=current.day, hour=hour, minute=minute)
 
+    if time<current_time():
+        await message.channel.send("이미 지난 시각입니다. 24시간제 표기를 사용해주세요.")
+        return
+
     current_game=Internal(opener, time)
 
     msg="@everyone\n{} 내전 신청이 열렸습니다.\n개최자: {}".format(str(current_game.time)[:-3], current_game.opener.mention)
@@ -145,6 +149,10 @@ async def 시간변경(message):
         minute=int(time[1])
     time=datetime(year=current.year, month=current.month, day=current.day, hour=hour, minute=minute)
 
+    if time<current_time():
+        await message.channel.send("이미 지난 시각입니다. 24시간제 표기를 사용해주세요.")
+        return
+    
     prev_time, current_game.time=current_game.time, time
 
     msg="@everyone\n{} 내전이 {}로 변경되었습니다.\n개최자: {}".format(str(prev_time)[:-3], str(current_game.time)[:-3], current_game.opener.mention)
