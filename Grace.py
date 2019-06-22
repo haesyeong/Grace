b2f4e47a67c0f505b3d90bee6d15ef4ae5894141
whitepp <dana_bak@naver.com>
2019-06-22 21:16:52 +0900

    remove code for channel restriction

--- Grace.py
+++ Grace.py
@@ -29,8 +29,6 @@ async def on_message(message):
     content = message.content
     channel = message.channel
 
-    if channel.id!=486550288686120961: return
-
     print('{} / {}: {}'.format(channel, author, content))
 
     if message.content.startswith(">>"):
