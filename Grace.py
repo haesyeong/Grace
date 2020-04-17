53fdb08af90228f5d5d1526f38353bd373672ee8
n0n3x1573n7 <n0n3x1573n7@users.noreply.github.com>
2020-04-17 23:00:39 +0900

    indent

--- Grace.py
+++ Grace.py
@@ -45,7 +45,7 @@ def has_role(member, role):
     return role in map(lambda x:x.name, member.roles)
 
 async def get_member_by_battletag(battletag):
-	global grace
+    global grace
     grace=client.get_guild(359714850865414144)
 
     for member in grace.members:
