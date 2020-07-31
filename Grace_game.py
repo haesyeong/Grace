6e99ef5b391dbca270946fac656ffe53c6419bc2
n0n3x1573n7 <n0n3x1573n7@users.noreply.github.com>
2020-07-31 13:29:28 +0900

    Update Grace_game.py

--- Grace_game.py
+++ Grace_game.py
@@ -449,7 +449,9 @@ async def 내전종료(message):
             continue
     log+='\n\n내전 신청자 총 {}명'.format(cnt-1)
 
+    print(current_game.get_game(), current_game.get_game()=='오버워치')
     if current_game.get_game()=='오버워치':
+        print("Leaving record...")
         await current_game.leave_record()
 
     await current_game.close()
