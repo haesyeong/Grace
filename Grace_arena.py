a1f004f2b69ebb1dc7f0d8160b90a4aada7419cc
n0n3x1573n7 <n0n3x1573n7@users.noreply.github.com>
2020-10-09 23:48:26 +0900

    Update Grace_arena.py

--- Grace_arena.py
+++ Grace_arena.py
@@ -177,7 +177,6 @@ async def get_arena_game(ws=None):
 async def change_arena_game(ws=None):
     if ws==None:
         ws=await get_worksheet(sheet_name=win_record,addr='https://docs.google.com/spreadsheets/d/1gfSsgM_0BVqnZ02ZwRsDniU-qkRF0Wo-B7rJhYoYXqc/edit#gid=174260089')
-    this=get_arena_game(ws)
     print(this)
     game=['오버워치','발로란트']
     game.remove(this)
