e0d7dc6f1d7312115db3d2303e690cb6518678d9
n0n3x1573n7 <n0n3x1573n7@users.noreply.github.com>
2021-06-10 22:25:11 +0900

    Update Grace_level.py

--- Grace_level.py
+++ Grace_level.py
@@ -51,23 +51,30 @@ async def 출석(message):
 async def 안녕(message):
     user=author(message)
     targets=content(message).split()[1:]
+    replies=[]
+    await message.delete()
     for target in targets:
         ws=ws_f.get_worksheet('responses')
         cols=ws_f.get_col_order(ws)
         row_idx=ws_f.search(ws, 'mention', user.mention, cols=cols)
         row=ws_f.get_row(ws, row_idx, cols=cols)
         if target==user:
-            await message.channel.send(f'본인에게는 사용할 수 없습니다.')
-        elif level(int(row['exp']))>=10:
-            await message.channel.send(f'신입 클랜원에게만 사용할 수 있습니다.')
+            reply=await message.channel.send(f'본인에게는 사용할 수 없습니다.')
+        elif ws_f.level(int(row['exp']))>=10:
+            reply=await message.channel.send(f'신입 클랜원에게만 사용할 수 있습니다.')
         elif user.mention in row['exp_get'].split():
-            await message.channel.send(f'이미 경험치를 한번 지급했습니다.')
+            reply=await message.channel.send(f'이미 경험치를 한번 지급했습니다.')
         elif len(row['exp_get'].split(','))>=hello_limit:
             await ws_f.give_exp(ws, 0, client, row_idx=row_idx, cols=cols, add_giver=user.mention)
-            await message.channel.send(f'{user.mention}님이 {target}님께 인사합니다.')
+            reply=await message.channel.send(f'{user.mention}님이 {target}님께 인사합니다.')
         else:
             await ws_f.give_exp(ws, hello_exp, client, row_idx=row_idx, cols=cols, add_giver=user.mention)
-            await message.channel.send(f'{user.mention}님이 {target}님께 인사하며 경험치를 줍니다.')
+            reply=await message.channel.send(f'{user.mention}님이 {target}님께 인사하며 경험치를 줍니다.')
+        replies.append(reply)
+    await asyncio.sleep(0.5)
+    for reply in replies:
+        await reply.delete()
+
 ############################################################
 #자동 기록(이벤트)
 @client.event
